3395b532be3779c009e55da50c232ee9d97fe1c3
lauryne <lauryne.assa@gmail.com>
2025-06-25 12:53:43 -0400

    fix github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import {Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Aime from './pages/Aime';
