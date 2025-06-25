4e36c861d941117af72f1b6fc5a5500f2c694fc1
lauryne <lauryne.assa@gmail.com>
2025-06-25 13:01:24 -0400

    changing routing for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ScrollToTop from './Scroll.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
